4b25f473ee5b5eebcfa8871361a705efbfd8d24e
Jesse Shipley <jesse.radack@gmail.com>
2017-02-23 11:35:02 -0700

    add json render, status 201 response, spec passes

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,10 +8,19 @@ class Api::V1::ItemsController < ApplicationController
     render json: Item.find(params[:id])
   end
 
+  def create
+    item = Item.new(item_params)
+    render json: item, status: 201
+  end
+
   def destroy
     item = Item.find(params[:id])
     item.destroy
     render json: "removed", status: :no_content
   end
 
+  private
+    def item_params
+      params.permit(:name, :description)
+    end
 end
