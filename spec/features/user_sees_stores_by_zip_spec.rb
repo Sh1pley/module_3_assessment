e5d9414a4ad75bf9832090bf998df4c348748e0b
Jesse Shipley <jesse.radack@gmail.com>
2017-02-23 09:13:57 -0700

    adding spec for 3rd party api, adding assertions before coding

---
+++ spec/features/user_sees_stores_by_zip_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+# As a user
+# And I should see the long name, city, distance,
+# phone number and store type for each of the 10 results
+
+describe "user can search stores by zipcode" do
+  it 'will return results from default distance limited to 10 per page' do
+# When I visit "/"
+    visit "/"
+# And I fill in a search box with "80202" and click "search"
+    fill_in "q", with: "80202"
+# And I should see stores within 25 miles of 80202
+# And I should see a message that says "16 Total Stores"
+  expect(page).to have_content("16 Total Stores")
+# And I should see exactly 10 results
+  stores = Array.new
+  stores = find('#parent ul').all('li')
+
+  expect(stores.size).to eq(10)
+# Then my current path should be "/search" (ignoring params)
+
+  end
+end
