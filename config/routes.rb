0defbb7ba47fc6e773ebd200cdaa70c011a065d2
Jesse Shipley <jesse.radack@gmail.com>
2017-02-23 11:40:06 -0700

    add null_session to appcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  namespace "api" do
-    namespace "v1" do
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
       resources :items, only: [:index, :show, :destroy, :create]
     end
   end
