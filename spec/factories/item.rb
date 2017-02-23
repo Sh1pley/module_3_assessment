a0ef859041423224e9d67528e04ae0bfc393e66f
Jesse Shipley <jesse.radack@gmail.com>
2017-02-23 10:55:20 -0700

    add factory, test factory with pry

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
   factory :item do
-    
+    name        {Faker::Superhero.power}
+    description {Faker::Superhero.name}
+    image_url   {"http://lorempixel.com/400/200"}
+  end
 end
--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe '/api/v1/items' do
   before do
-    Item.create()
+    @items = create_list(:item, 20)
+  end
+  
   it 'should respond with 200' do
     get "/api/v1/items"
 
