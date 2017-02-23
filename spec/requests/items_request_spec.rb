f5c1d57db5ec46ab52667f820def37d57492d2ca
Jesse Shipley <jesse.radack@gmail.com>
2017-02-23 11:01:11 -0700

    spec passes up to expecting not to have created/updated at, adding serializers next

--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe '/api/v1/items' do
   before do
-    @items = create_list(:item, 20)
+    @items_list = create_list(:item, 20)
   end
-  
+
   it 'should respond with 200' do
     get "/api/v1/items"
 
@@ -15,6 +15,15 @@ RSpec.describe '/api/v1/items' do
     get '/api/v1/items'
 
     items = response.body
-    binding.pry
+    expect(items).to be_a(String)
+
+    parsed_items = JSON.parse(items)
+    
+    expect(parsed_items.first).to have_key "id"
+    expect(parsed_items.first).to have_key "name"
+    expect(parsed_items.first).to have_key "description"
+
+    expect(parsed_items.first).to_not have_key "created_at"
+    expect(parsed_items.first).to_not have_key "updated_at"
   end
 end
