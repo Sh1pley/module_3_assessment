f8854c13c3d38300d319e3b8f7ebe69359740189
Jesse Shipley <jesse.radack@gmail.com>
2017-02-23 12:04:10 -0700

    add refactor as bestbuy service item, needs further polishing

--- app/models/bestbuy_service.rb
+++ app/models/bestbuy_service.rb
@@ -2,14 +2,13 @@ class BestbuyService
 
 # "https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=#{ENV['bestbuy_api']}&format=json")
   def self.search(request)
-   conn = Faraday.new(:url => "https://api.bestbuy.com/v1/stores") do |f|
+   conn = Faraday.new(:url => "https://api.bestbuy.com/v1/stores((area(#{request},25)))") do |f|
       f.request :url_encoded
       f.response :logger
       f.adapter Faraday.default_adapter
     end
-    conn.get {areaFunction: "(area(#{request},25))",
-              apiKey: ENV['bustbuy_api'],
-              responseFormat: "&format=jason"}
+  conn.get {:apiKey => ENV['bustbuy_api'],
+            :format => "json"}
   end
 
 end
